57b8ea9d87f9e80d549398d868676c35120427a1
stewartamiles <stewartamiles@40f4469a-5155-0410-be90-2de3f0bae501>
2009-07-23 16:33:33 +0000

    Fixed cmockery "Issue 9: assert_macro.c"
       Reported by khb.hnu, Apr 12, 2009
       Line30:
    
       for (i = 0; i < sizeof(status_code_string) / sizeof(status_code_string[0]);    
       change it as follow:
    
       for (i = 0; i < sizeof(status_code_strings) / sizeof(status_code_strings[0]);
    
    
    
    git-svn-id: https://cmockery.googlecode.com/svn@33 40f4469a-5155-0410-be90-2de3f0bae501

--- trunk/src/example/assert_macro.c
+++ trunk/src/example/assert_macro.c
@@ -27,8 +27,8 @@ const char* get_status_code_string(const unsigned int status_code) {
 
 unsigned int string_to_status_code(const char* const status_code_string) {
     unsigned int i;
-    for (i = 0; i < sizeof(status_code_string) / sizeof(status_code_string[0]);
-         i++) {
+    for (i = 0; i < sizeof(status_code_strings) /
+                    sizeof(status_code_strings[0]); i++) {
         if (strcmp(status_code_strings[i], status_code_string) == 0) {
             return i;
         }
