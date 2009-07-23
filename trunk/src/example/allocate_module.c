d15a516dd1758aebde86da4d91757ce74c5525b9
stewartamiles <stewartamiles@40f4469a-5155-0410-be90-2de3f0bae501>
2009-07-23 16:36:01 +0000

    Deleted all trailing whitespace from cmockery source files.
    git-svn-id: https://cmockery.googlecode.com/svn@36 40f4469a-5155-0410-be90-2de3f0bae501

--- trunk/src/example/allocate_module.c
+++ trunk/src/example/allocate_module.c
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,7 +23,7 @@
 
 #if UNIT_TESTING
 extern void* _test_malloc(const size_t size, const char* file, const int line);
-extern void* _test_calloc(const size_t number_of_elements, const size_t size, 
+extern void* _test_calloc(const size_t number_of_elements, const size_t size,
                           const char* file, const int line);
 extern void _test_free(void* const ptr, const char* file, const int line);
 
